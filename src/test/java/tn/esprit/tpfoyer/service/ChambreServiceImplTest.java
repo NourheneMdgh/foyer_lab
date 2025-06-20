1de691254d04860c409a4b4d437e8a8aeb66f7e7
chaima.baraket <chaima.baraket@pyxis.com.tn>
2025-06-20 13:46:56 +0100

    remove import entity bloc for security issue sonar

--- src/test/java/tn/esprit/tpfoyer/service/ChambreServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/service/ChambreServiceImplTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tn.esprit.tpfoyer.entity.Bloc;
+// import tn.esprit.tpfoyer.entity.Bloc;
 import tn.esprit.tpfoyer.entity.Chambre;
 import tn.esprit.tpfoyer.entity.Reservation;
 import tn.esprit.tpfoyer.entity.TypeChambre;
